9f31a30ad616b4c0c1f92d5da582ce6a18304ff9
kharo019 <kevinharo019@gmail.com>
2023-12-02 14:23:07 -0700

    fixing errors when on mobile devices

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,26 +22,18 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link, v
 					scale: 1,
 					speed: 450
 				}}
-				className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
-					<div className = "relative w-full h-[200px]">
+				className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full ">
+					<div className = "relative w-full h-[230px]">
 						<img 
 							src = {image} 
 							alt = {name}
 							className = "w-full h-full object-cover rounded-2xl"
 						/>
 						{/*GitHub and website link icon located top right of projects*/}
+						
 						<div className = "absolute inset-0 flex justify-end m-3 card-img_hover">
-							<div
-								onClick = {() => window.open(source_code_link, "_blank")}
-								className = "black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
-								<img 
-									src = {github}
-									alt = "github"
-									className = "w-1/2 h-1/2 object-contain"
-								/>
-							</div>
 
-							<div
+						<div
 								onClick = {() => window.open(visit, "_blank")}
 								className = "black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer">
 								<img 
@@ -49,9 +41,24 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link, v
 									alt = "github"
 									className = "w-1/2 h-1/2 object-contain"
 								/>
+							</div>		
+
+
+							<div
+								onClick = {() => window.open(source_code_link, "_blank")}
+								className = "black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer sm:w[50px]">
+								<img 
+									src = {github}
+									alt = "github"
+									className = "w-1/2 h-1/2 object-contain"
+								/>
 							</div>
+
 						</div>
 					</div>
+
+
+
 				<div className = "mt-5">
 					<h3 className = "text-white font-bold text-[24px]"> {name} </h3>
 					<p className = "mt-2 text-secondary text-[14px]"> {description} </p>
@@ -60,7 +67,7 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link, v
 				<div className = "mt-4 flex flex-wrap gap-2">
 					{tags.map((tag) => (
 						<p 
-						key = {`${tag.name}-${tag.name}`} 
+						key = {`${name}-${tag.name}`} 
 						className = {`text-[14px] ${tag.color}`}>
 							#{tag.name}
 						</p>
--- src/constants/index.js
+++ src/constants/index.js
@@ -356,137 +356,135 @@ const experiences = [
 
 
 
-		// {
-		// 	name: "AggieCompiler",
-		// 	description:
-		// 		"This was a Senior team-based project consisting of myself and another member. The project scope is to provide resources to incoming NMSU students as well as provide a system that professors can use for teaching purposes. This project consists of a data structure roadmap, Q&A system, and Admin/user system. [No website, just code source]",
-		// 	tags: [
-		// 		{
-		// 			name: "HTML",
-		// 			color: "light-blue-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "JavaScript",
-		// 			color: "orange-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "CSS",
-		// 			color: "red-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "Python",
-		// 			color: "green-text-gradient",
-		// 		},
+		{
+			name: "AggieCompiler",
+			description:
+				"This was a Senior team-based project consisting of myself and another member. The project scope is to provide resources to incoming NMSU students as well as provide a system that professors can use for teaching purposes. This project consists of a data structure roadmap, Q&A system, and Admin/user system. [No website, just code source]",
+			tags: [
+				{
+					name: "HTML",
+					color: "light-blue-text-gradient",
+				},
+				{
+					name: "JavaScript",
+					color: "orange-text-gradient",
+				},
+				{
+					name: "CSS",
+					color: "red-text-gradient",
+				},
+				{
+					name: "Python",
+					color: "green-text-gradient",
+				},
 				
-		// 		],
-		// 		image: AggieCompiler,
-		// 		source_code_link: 'https://github.com/sfransis/AggieCompiler',
-		// 		visit: 'https://github.com/sfransis/AggieCompiler',
-		// },
+				],
+				image: AggieCompiler,
+				source_code_link: 'https://github.com/sfransis/AggieCompiler',
+				visit: 'https://github.com/sfransis/AggieCompiler',
+		},
 
 
 
-		// {
-		// 	name: "ACI (Animal Computation Interaction)",
-		// 	description:
-		// 		"This was a solo project in Visual Programming and the purpose of this object was to be an interactive game with your cat that outputs a wind generator using Pure Data, with the ability to change the frequency of the wind. [No website, just code source]",
-		// 	tags: [
-		// 		{
-		// 			name: "python",
-		// 			color: "green-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "pd~ (PureData)",
-		// 			color: "black-gradient",
-		// 		},
+		{
+			name: "ACI (Animal Computation Interaction)",
+			description:
+				"This was a solo project in Visual Programming and the purpose of this object was to be an interactive game with your cat that outputs a wind generator using Pure Data, with the ability to change the frequency of the wind. [No website, just code source]",
+			tags: [
+				{
+					name: "python",
+					color: "green-text-gradient",
+				},
+				{
+					name: "pd~ (PureData)",
+					color: "black-gradient",
+				},
 					
-		// 		{
-		// 			name: "TypeScript",
-		// 			color: "light-blue-text-gradient",
-		// 		},
-		// 		],
+				{
+					name: "TypeScript",
+					color: "light-blue-text-gradient",
+				},
+				],
 				
-		// 		image: ACI,
-		// 		source_code_link: 'https://github.com/kharo019/ACI',
-		// 		visit: 'https://github.com/kharo019/ACI',
-		// },
+				image: ACI,
+				source_code_link: 'https://github.com/kharo019/ACI',
+				visit: 'https://github.com/kharo019/ACI',
+		},
 
 
 
 		
-		// {
-		// 	name: "Using spark to cluster a COVID-19 document into 8 clusters using AWS",
-		// 	description:
-		// 		"This project involved meticulous data cleaning, focusing on removing noise such as numeric values, special characters, and common stop words. Simultaneously, an AWS architecture was set up, utilizing Amazon EC2 for secure access and Amazon EMR for scalable processing, with Spark serving as the framework for data analysis. This comprehensive approach, combining data cleaning, AWS architecture setup, and Spark processing, demonstrated effective extraction of meaningful insights from the CORD-19 dataset. By effectively combining data cleaning, AWS architecture setup, and Spark processing, the project demonstrates a comprehensive approach to extracting meaningful insights from the COVID-19 dataset. [No website, just code source]",
-		// 	tags: [
-		// 		{
-		// 			name: "AWS",
-		// 			color: "red-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "Python",
-		// 			color: "green-text-gradient",
-		// 		},
+		{
+			name: "Using spark to cluster a COVID-19 document into 8 clusters using AWS",
+			description:
+				"This project involved meticulous data cleaning, focusing on removing noise such as numeric values, special characters, and common stop words. Simultaneously, an AWS architecture was set up, utilizing Amazon EC2 for secure access and Amazon EMR for scalable processing, with Spark serving as the framework for data analysis. This comprehensive approach, combining data cleaning, AWS architecture setup, and Spark processing, demonstrated effective extraction of meaningful insights from the CORD-19 dataset. By effectively combining data cleaning, AWS architecture setup, and Spark processing, the project demonstrates a comprehensive approach to extracting meaningful insights from the COVID-19 dataset. [No website, just code source]",
+			tags: [
+				{
+					name: "AWS",
+					color: "red-text-gradient",
+				},
+				{
+					name: "Python",
+					color: "green-text-gradient",
+				},
 				
-		// 		],
-		// 		image: AWS,
-		// 		source_code_link: 'https://github.com/kharo019/CloneGPT3',
-		// 		visit: 'https://clonegpt3.vercel.app',
-		// },
+				],
+				image: AWS,
+				source_code_link: 'https://github.com/kharo019/CloneGPT3',
+				visit: 'https://clonegpt3.vercel.app',
+		},
 
 
 
-		// {
-		// 	name: "Scallywags",
-		// 	description:
-		// 		"This was my first team-based project, consisted of creating a project website that would have promote workingout in a series of stages of beginner, intermediate, and expert level. It had a calendar, timer, and reminder system to remind the user either when the series of workout was over, or when the user had to start their training series, or on what specific day should the user focus on what muscle group to workout.  [No website, just code source]",
-		// 	tags: [
-		// 		{
-		// 			name: "HTML",
-		// 			color: "blue-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "Javascript",
-		// 			color: "orange-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "Java",
-		// 			color: "orange-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "Python",
-		// 			color: "green-text-gradient",
-		// 		},
+		{
+			name: "Scallywags",
+			description:
+				"This was my first team-based project, consisted of creating a project website that would have promote workingout in a series of stages of beginner, intermediate, and expert level. It had a calendar, timer, and reminder system to remind the user either when the series of workout was over, or when the user had to start their training series, or on what specific day should the user focus on what muscle group to workout.  [No website, just code source]",
+			tags: [
+				{
+					name: "HTML",
+					color: "blue-text-gradient",
+				},
+				{
+					name: "Javascript",
+					color: "orange-text-gradient",
+				},
+				{
+					name: "Java",
+					color: "orange-text-gradient",
+				},
+				{
+					name: "Python",
+					color: "green-text-gradient",
+				},
 				
-		// 		],
-		// 		image: Scallywags,
-		// 		source_code_link: 'https://github.com/kharo019/Scallywags/tree/main',
-		// 		visit: 'https://github.com/kharo019/Scallywags/tree/main',
-		// },
+				],
+				image: Scallywags,
+				source_code_link: 'https://github.com/kharo019/Scallywags/tree/main',
+				visit: 'https://github.com/kharo019/Scallywags/tree/main',
+		},
 
 
 
-
-
-		// {
-		// 	name: "Database-Hospital && School",
-		// 	description:
-		// 		"This was a solo project about administering multiple fictional data points about a hospital. The goal was to practice with mySQL, queries, and manipulating data across the database. Having to take care of edge cases especially with Primary Keys and Foreign Keys via command line.     [No website, just code source]",
-		// 	tags: [
-		// 		{
-		// 			name: "python",
-		// 			color: "green-text-gradient",
-		// 		},
-		// 		{
-		// 			name: "mySQL",
-		// 			color: "pink-text-gradient",
-		// 			},
+		{
+			name: "Database-Hospital && School",
+			description:
+				"This was a solo project about administering multiple fictional data points about a hospital. The goal was to practice with mySQL, queries, and manipulating data across the database. Having to take care of edge cases especially with Primary Keys and Foreign Keys via command line.     [No website, just code source]",
+			tags: [
+				{
+					name: "python",
+					color: "green-text-gradient",
+				},
+				{
+					name: "mySQL",
+					color: "pink-text-gradient",
+					},
 				
-		// 		],
-		// 		image: DB,
-		// 		source_code_link: 'https://github.com/kharo019/Hospital-Database',
-		// 		visit: 'https://github.com/kharo019/Hospital-Database',
-		// },
+				],
+				image: DB,
+				source_code_link: 'https://github.com/kharo019/Hospital-Database',
+				visit: 'https://github.com/kharo019/Hospital-Database',
+		},
 
 
 		];
