d65bee40ac60df6ee3dd5c38d24fa67a565207b3
kharo019 <kevinharo019@gmail.com>
2024-01-12 15:09:15 -0700

    another set of small changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -20,15 +20,15 @@ import { fadeIn, textVariant } from '../utils/motion';
 const ProjectCard = ({index, name, description, tags, image, source_code_link, visit}) => {
 	console.log('Rendering ProjectCard:', name);
 	return (
-		<motion.div variants = {fadeIn("up", "spring", index * 1, 0.75)}>
+		<motion.div variants = {fadeIn("up", "spring", index * 0.5, 0.75)}>
 			<Tilt
 				options = {{
 					max: 45,
 					scale: 1,
 					speed: 450,
 				}}
-				className = "bg-tertiary p-4 rounded-2xl sm:w-[280px] w-full ">
-					<div className = "relative w-full h-[200px]">
+				className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+					<div className = "relative w-full h-[230px]">
 						<img 
 							src = {image} 
 							alt = {name}
